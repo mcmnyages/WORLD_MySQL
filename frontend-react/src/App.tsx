1e0405c6b9d981dd5c3317dfb6924ef0eb1a3046
McMnyages <morachasilas@gmail.com>
2025-07-24 13:04:47 +0300

    feat: add routing for CitiesList and CountryLanguagesList components

--- frontend-react/src/App.tsx
+++ frontend-react/src/App.tsx
@@ -1,13 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import TopNav from "./components/layout/TopNav";
 import CountryList from "./pages/CountryList";
-
+import CitiesList from "./pages/CitiesList";
+import CoutryLanguagesList from "./pages/CountryLangiagesList";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="p-4">
+        <TopNav />
         <Routes>
           <Route path="/" element={<CountryList />} />
+          <Route path="/cities" element={<CitiesList />} />
+          <Route path="countries" element={<CountryList />} />
+          <Route path="/country-languages" element={<CoutryLanguagesList />} />
         </Routes>
       </div>
     </BrowserRouter>
