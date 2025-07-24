fa5ea5174896998901d313e9e22977880113b317
McMnyages <morachasilas@gmail.com>
2025-07-24 12:56:40 +0300

    feat: update axios instance to use environment variable for base URL

--- frontend-react/src/api/axiosInstance.ts
+++ frontend-react/src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL:  'http://localhost:5000/api', // uses the value from .env
+  baseURL:  import.meta.env.VITE_API_BASE_URL , // uses the value from .env
   timeout: 10000, // optional: fail if request takes too long
 });
 
