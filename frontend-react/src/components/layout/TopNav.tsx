d692165a782c3a27edc909151774bce83189b24b
McMnyages <morachasilas@gmail.com>
2025-07-26 21:57:23 +0300

    refactor: update TopNav component to use Link for navigation

--- frontend-react/src/components/layout/TopNav.tsx
+++ frontend-react/src/components/layout/TopNav.tsx
@@ -1,3 +1,7 @@
+import { Link } from "react-router-dom";
+
+
+
 export default function TopNav(){
     return (
 
@@ -5,9 +9,11 @@ export default function TopNav(){
             <p>Start of  Top Nav</p>
             <nav>
                 <ul>
-                    <li>Countries</li>
-                    <li>Cities</li>
-                    <li>Country Languages</li>
+                    <Link to="/">Home</Link> <br />
+                    <Link to="/countries">Countries</Link> <br />
+                    <Link to="/cities">Cities</Link><br />
+                    <Link to="/country-languages">Country Languages</Link><br/>
+
                 </ul>
             </nav>
             <p>End of Top Nav</p>
