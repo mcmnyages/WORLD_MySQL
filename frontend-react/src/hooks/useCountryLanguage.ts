025ebb089f576afe08447d333ff40a203039646a
McMnyages <morachasilas@gmail.com>
2025-07-26 21:56:55 +0300

    refactor: update useCountryLanguage hook to use correct response type

--- frontend-react/src/hooks/useCountryLanguage.ts
+++ frontend-react/src/hooks/useCountryLanguage.ts
@@ -1,9 +1,9 @@
 import {useQuery} from '@tanstack/react-query';
 import { getAllCountryLanguages } from '../api/CountryLanguageApi';
-import type { CountryLanguage } from '../types/CountryLanguage';
+import type { CountryLanguageResponse } from '../types/CountryLanguage';
 
 export const useCountryLanguage = () =>{
-    return useQuery<CountryLanguage[]>({
+    return useQuery<CountryLanguageResponse>({
         queryKey: ['AllCountries'],
         queryFn: getAllCountryLanguages,
     });
