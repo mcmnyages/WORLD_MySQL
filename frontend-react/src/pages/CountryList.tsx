66f74ac035ed42ef6f66d57cd57af9bf18f0b0ee
McMnyages <morachasilas@gmail.com>
2025-07-27 22:13:11 +0300

    feat: integrate search functionality and simplify pagination controls in CountryList component

--- frontend-react/src/pages/CountryList.tsx
+++ frontend-react/src/pages/CountryList.tsx
@@ -1,67 +1,53 @@
 import { useState } from 'react';
 import { useCountries } from '../hooks/useCountries';
 import CountryListItem from '../components/Country/CountryListItem';
+import {useSearch} from '../hooks/useSearch';
+import SearchInput from '../components/SearchInput';
+import Pagination from '../components/ui/Pagination/Pagination';
 
 const CountryList: React.FC = () => {
 
   const [page, setPage] = useState(1);
-  const { data, isLoading, error, isFetching } = useCountries({ page, pageSize: 10 });
+ const { search, debouncedSearch, handleSearchChange } = useSearch();
+  const { data, isLoading, error } = useCountries({ page, pageSize: 10, name: debouncedSearch });
 
   if (isLoading) return <p>Loading...</p>;
   if (error) return <p>Error loading countries</p>;
 
-  const goFirst=()=> setPage(1);
-  const goLast = () => {
-    if (data?.pagination?.totalPages) {
-      setPage(data.pagination.totalPages);
-    }
-  }
+ 
 
-  const goPrev = () => setPage((prev) => Math.max(prev - 1, 1));
-  const goNext = () => {
-    if (data?.pagination?.totalPages) {
-      setPage((next) => Math.min(next + 1, data.pagination.totalPages));
-    }
-  }
+  
 
   return (
     <div>
       <p>List of Countries:</p>
-      <p>Total: {data?.pagination.total}</p>
 
+      <SearchInput
+        value={search}
+        onChange={(e) => {
+          handleSearchChange(e);
+          setPage(1); // Reset page when searching
+        }}
+        placeholder="Search countries..."
+      />
+
+      <p>Total: {data?.pagination.total}</p>
+      <div> 
+      </div>
       <ul>
         {data?.countries?.map((country) => (
           <CountryListItem key={country.Code} country={country} />
         ))}
       </ul>
-
-      <div>
-        <button onClick={goFirst} disabled={page === 1}>
-          First
-        </button>
-        <button
-          onClick={goPrev }
-          disabled={page === 1}
-        >
-          Prev
-        </button>
-
-        <span >
-          Page {page} of {data?.pagination.totalPages}
-        </span>
-
-        <button
-          onClick={goNext}
-          disabled={page === data?.pagination.totalPages}
-        >
-          Next
-        </button>
-        <button onClick={goLast} disabled={page === data?.pagination.totalPages}>
-          Last
-        </button>
-
-        {isFetching && <p>Updating...</p>}
-      </div>
+      <Pagination
+        currentPage={page}
+        totalPages={data?.pagination.totalPages ?? 1}
+        onPageChange={setPage}
+        isLoading={isLoading}
+        pageSize={10}
+        onPageSizeChange={(size) => console.log(`Page size changed to: ${size}`)}
+      />
+      <br />
     </div>
   );
 };
