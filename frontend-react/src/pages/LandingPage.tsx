ee4b37cd573ef304796d3def14144d95c1fb4867
McMnyages <morachasilas@gmail.com>
2025-07-27 22:24:50 +0300

    feat: enhance LandingPage layout with improved styling and content

--- frontend-react/src/pages/LandingPage.tsx
+++ frontend-react/src/pages/LandingPage.tsx
@@ -2,10 +2,11 @@
 
 export default function LandingPage(){
     return(
-        <div className="landing-page">
-            <h1>Welcome to the World Facts</h1>
-            <p>Explore countries, languages, and more!</p>
-        
+        <div className="landing-page mt-120px flex flex-col items-center justify-center text-center">
+         <h1 className="text-8xl font-bold">Welcome to the World Facts</h1>
+            <p className="text-5xl mt-4 ">Discover information about countries, languages, and cities.        
+            </p>
+            <span className="text-4xl">Inspired by MySQL world database and the data was from around 1990s and 2000s</span>
         </div>
     )
 }
